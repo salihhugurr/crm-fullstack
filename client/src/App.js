6ef488e8f841b724a669538540ba4cdd82be6d7c
salihhugurr <salihugur46@gmail.com>
2022-12-16 20:20:13 +0300

    Products page & service added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import React,{useMemo} from "react";
+import React, { useMemo } from "react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useSelector } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
-import Dashboard from "scenes/dashboard"
-import Layout from "scenes/layout"
+import Dashboard from "scenes/dashboard";
+import Layout from "scenes/layout";
+import Products from "scenes/products";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -17,8 +18,12 @@ function App() {
           <CssBaseline />
           <Routes>
             <Route element={<Layout />}>
-              <Route path="/" element={<Navigate to={"/dashboard"} replace/>}/>
-              <Route path="/dashboard" element={<Dashboard />}/>
+              <Route
+                path="/"
+                element={<Navigate to={"/dashboard"} replace />}
+              />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
             </Route>
           </Routes>
         </ThemeProvider>
